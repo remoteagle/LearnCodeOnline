9b02dd04716a1552f4f389634f8815a2e96ed731
omertabibi <omertabibi7@gmail.com>
2019-01-21 14:25:39 +0430

    add files

---
+++ app/src/main/java/com/example/omer/learncodeonline/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.omer.learncodeonline;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void RegisterMe(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+}
